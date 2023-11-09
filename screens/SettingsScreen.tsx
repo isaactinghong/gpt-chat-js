26dba1779f43ea19d36d7703ea30ce39f3a440b8
Isaac Chan <isaactinghong@gmail.com>
2023-11-09 22:59:42 +0800

    Add openAI API key input and save button functionality

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { useDispatch } from "react-redux";
+import { saveSettings } from "../state/actions/settingsActions";
+import { useSelector } from "react-redux";
+import { AppState } from "../state/states/app-state";
 
 const SettingsScreen = () => {
+  const dispatch = useDispatch();
+
+  const openAiApiKey = useSelector(
+    (state: AppState) => state.settings.openAiApiKey
+  );
+
+  const [openAiApiKeyLocal, setOpenAiApiKey] = React.useState(openAiApiKey);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>API Key</Text>
-      <TextInput style={styles.input} placeholder="Your API Key here" />
-      <Button title="Save" color={"#000"} onPress={() => {}} />
+      <TextInput
+        style={styles.input}
+        placeholder="Your API Key here"
+        value={openAiApiKeyLocal}
+        onChangeText={setOpenAiApiKey}
+      />
+      <Button
+        title="Save"
+        color={"#000"}
+        onPress={() => {
+          /* handle save, using action: saveSettings */
+          dispatch(
+            saveSettings({
+              openAiApiKey: openAiApiKeyLocal,
+            })
+          );
+        }}
+      />
     </View>
   );
 };
