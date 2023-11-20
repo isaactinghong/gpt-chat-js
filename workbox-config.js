b58c006f2ce9c5e59a9194fdb53e75211dcd13e0
Isaac Chan <isaactinghong@gmail.com>
2023-11-20 23:53:38 +0800

    Update workbox-config.js to exclude specific files from globIgnores

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,10 @@
 module.exports = {
-	globDirectory: 'web-build/',
-	globPatterns: [
-		'**/*.{json,png,ico,html,js,txt,ttf}'
-	],
-	swDest: 'web-build/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  globDirectory: "web-build/",
+  globPatterns: ["**/*.{json,png,ico,html,js,txt,ttf}"],
+  globIgnores: [
+    "**/.1feff74faaf0efc6a044", // Add a pattern here that matches the file to exclude
+    "_redirects",
+  ],
+  swDest: "web-build/sw.js",
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
